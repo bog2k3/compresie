71444244d04e6d0bc01f0a7c38fae1431a738790
bogdan ionita <bog2k3@gmail.com>
2016-04-17 12:53:31 +0300

    tree builder

--- TreeBuilder.cpp
+++ TreeBuilder.cpp
@@ -6,7 +6,31 @@
  */
 
 #include "TreeBuilder.h"
+#include <algorithm>
+
+TreeNode* TreeBuilder::createNode(std::vector<std::pair<char, double>>::iterator it) {
+	TreeNode* pNode = new TreeNode('\0', true);
+	pNode->pLeft = new TreeNode(it->first, false);
+	return pNode;
+}
 
 TreeNode* TreeBuilder::buildHuffmanTree(std::map<char, double> const& stats) {
-	return nullptr;
+	// transformam dictionarul intr-o lista:
+	std::vector<std::pair<char, double>> statList;
+	for (auto p : stats)
+		statList.push_back(p);
+	// sortam lista dupa numarul de aparitii (cel mai mare la inceput)
+	std::sort(statList.begin(), statList.end(), [](std::pair<char, double> &l, std::pair<char, double> &r) {
+		return l.second >= r.second;
+	});
+
+	auto it = statList.begin();
+	TreeNode* root = createNode(it);
+	TreeNode* last = root;
+	while (it != statList.end()) {
+		last->pRight = createNode(it++);
+		last = last->pRight;
+	}
+
+	return root;
 }
