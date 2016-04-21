ed8225a6ee8d15cfc21a2584bdbee9ee311c7824
bogdan@debianVM <bog2k3@gmail.com>
2016-04-21 14:05:53 +0300

    fixat decompresie

--- Decompressor.cpp
+++ Decompressor.cpp
@@ -8,32 +8,55 @@
 #include "Decompressor.h"
 #include "dictionaryMapElement.h"
 #include <map>
+#include <fstream>
 
 void Decompressor::decompressToFile(std::istream &input, std::string const& outFileName) {
 	// 1. citim dictionarul
 	unsigned int dictSize;
 	input.read((char*)&dictSize, sizeof(dictSize));
-	std::map<std::vector<bool>, char> dict;
+	std::map<std::string, char> dict;
 	for (unsigned i=0; i<dictSize; i++) {
 		mapElement e;
 		e.readFrom(input);
 		dict[makeBitSequence(e.bytes, e.numBits)] = e.ch;
 	}
 
-	return;
-
 	// 2. citim continutul
+	std::vector<unsigned char> buffer;
+	while (!input.eof()) {
+		unsigned char c;
+		input.read((char*)&c, sizeof(c));
+		buffer.push_back(c);
+	}
+
+	// transformam in secventa de biti:
+	std::string bits = makeBitSequence(buffer, buffer.size() * 8);
+
+	// interpretam cuvintele de cod si scriem caracterele asociate in fisierul de iesire:
+	std::ofstream out(outFileName);
+
+	std::string cuvantulCurent;
+	auto it = bits.begin();
+	while (it != bits.end()) {
+		cuvantulCurent.push_back(*it);
+		it++;
+		if (dict.find(cuvantulCurent) != dict.end()) {
+			// cuvantul este complet
+			out << dict[cuvantulCurent];
+			cuvantulCurent.clear();
+		}
+	}
 }
 
-std::vector<bool> Decompressor::makeBitSequence(std::vector<unsigned char> const& bytes, int numBits) {
-	std::vector<bool> bits;
+std::string Decompressor::makeBitSequence(std::vector<unsigned char> const& bytes, int numBits) {
+	std::string bits;
 	int crtByte = 0, crtBit = 7;
 	for (int i=0; i<numBits; i++) {
 		if (crtBit < 0) {
 			crtByte++;
 			crtBit = 7;
 		}
-		bits.push_back((bytes[crtByte] & (1 << crtBit)) != 0);
+		bits.push_back((bytes[crtByte] & (1 << crtBit)) != 0 ? '1' : '0');
 		crtBit--;
 	}
 	return bits;
